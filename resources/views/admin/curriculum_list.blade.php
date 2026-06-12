be9f48eda88046bf4e82b2d457baa276de7530bd
MiyaiKasane <white.snow.ice.girl@gmail.com>
2026-06-12 14:09:53 +0900

    0612UpDate

--- resources/views/admin/curriculum_list.blade.php
+++ resources/views/admin/curriculum_list.blade.php
@@ -34,17 +34,58 @@
             </p>
         </div>
         
-        <div class="grade_type">
-            <button type="submit"></button>
-            <!-- ＠foreach ( as )　各学年がはいる
-                <div>
-                    <p></p>
+        <div class="curriculum_content">
+
+            <div class="grade_type">
+                <button type="submit" class="pschool" data-id=>小学校2年生</button>
+                <button type="submit" class="pschool">小学校3年生</button>
+                <button type="submit" class="pschool">小学校4年生</button>
+                <button type="submit" class="pschool">小学校5年生</button>
+                <button type="submit" class="pschool">小学校6年生</button>
+                <button type="submit" class="jhschool">中学校1年生</button>
+                <button type="submit" class="jhschool">中学校2年生</button>
+                <button type="submit" class="jhschool">中学校3年生</button>
+                <button type="submit" class="hschool">高校1年生</button>
+                <button type="submit" class="hschool">高校2年生</button>
+                <button type="submit" class="hschool">高校3年生</button>
+
+                <!-- ＠foreach ( as )　各学年がはいる
+                    <div>
+                        <p></p>
+                    </div>
+                ＠endforeach -->
+            </div>
+        
+            <div class="curriculum_article">
+                <div class="article_cell">
+                    <p>AA</p>
+                    <p>AA</p>
+                    <button type="submit" class="edit_btn">授業内容編集</button>
+                    <button type="submit" class="schedule_btn">配信日時編集</button>
                 </div>
-            ＠endforeach -->
-        </div>
-      
-        <div class=>
-            <p>＊＊</p>
+
+                <div class="article_cell">
+                    <p>AA</p>
+                    <p>AA</p>
+                    <button type="submit" class="edit_btn">授業内容編集</button>
+                    <button type="submit" class="schedule_btn">配信日時編集</button>
+                </div>
+                
+                <div class="article_cell">
+                    <p>AA</p>
+                    <p>AA</p>
+                    <button type="submit" class="edit_btn">授業内容編集</button>
+                    <button type="submit" class="schedule_btn">配信日時編集</button>
+                </div>
+
+                <div class="article_cell">
+                    <p>AA</p>
+                    <p>AA</p>
+                    <button type="submit" class="edit_btn">授業内容編集</button>
+                    <button type="submit" class="schedule_btn">配信日時編集</button>
+                </div>
+            </div>
+
         </div>
     </div>
 
